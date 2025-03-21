138554b6a005f1ab7b5db3e57aabe3edd71d1522
Mohammad Adnan <amd642038@gmail.com>
2025-03-21 20:17:10 +0600

    Create Code-15.c

---
+++ Code-15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("Celsius to Fahrenheit: F = (C * 9/5) + 32\n");
+    printf("Fahrenheit to Celsius: C = (F - 32) * 5/9\n");
+    printf("Area of Triangle: A = 0.5 * base * height\n");
+    printf("Area of Circle: A = π * r^2\n");
+    printf("Circumference of Circle: C = 2 * π * r\n");
+    printf("Inch to CM: CM = Inch * 2.54\n");
+    printf("CM to Inch: Inch = CM / 2.54\n");
+    printf("Square: S = num^2\n");
+    printf("Square Root: SR = sqrt(num)\n");
+    printf("Area of Rectangle: A = length * width\n");
+    return 0;
+}
